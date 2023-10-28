1178a99266a332849dab1752a0da9811af822de2
Claudir Santos <102737368+TerrorK1ng@users.noreply.github.com>
2023-10-28 00:54:18 +0000

    Termin

---
+++ main.py
@@ -0,0 +1,14 @@
+cpf = int(input("Digite seu CPF: \n"))
+idade = int(input("Digite sua idade: \n"))
+nome = input("Digite seu nome: \n")
+genero = bool(input("Selecione seu genero: 0 - Masculino   1 - Feminino: \n"))
+Salario = int(input("Coloque seu salário mensal: \n"))
+emprego = bool(input("Esta trabalhando: 0- Desempregado   1- Trabalhando: \n"))
+deficiencia = bool(input("PCD: 0- Nao   1-Sim: \n"))
+mae = input("Nome da Mae: \n")
+pai = input("Nome do Pai: \n")
+# Ponto da cor
+print("""
+    Cadastro de pessoa fisica:
+    Nome:
+    """)
